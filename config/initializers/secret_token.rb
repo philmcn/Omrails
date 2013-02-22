5520f2b6381cbfa3002ad33ca90ac8bd8d980a96
Philip McNamara <phil@tapmap.com>
2013-02-21 22:19:26 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '099d54185916f8fe091c6660d4254ac4b3caa158ec40125dc2383a118ce818e5d459ef05be6c9d9a9ebb0e89b9a655cf3a80750157bdedc4fb6e6cb3b20839c3'
